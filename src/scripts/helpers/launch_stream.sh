67cd8f9292aa89ef4e72d1db74e0a279d11ad702
José Luis Garrido Labrador <jlgarrido97@gmail.com>
2020-07-02 13:51:01 +0200

    Consumer detached

--- src/scripts/helpers/launch_stream.sh
+++ src/scripts/helpers/launch_stream.sh
@@ -8,7 +8,7 @@ OPTIONSCONSUMER="$3"
 #Necesario para nvidia
 #docker exec fis-hubu-consumer-$ID rmmod nvidia_drm && rmmod nvidia_modeset && rmmod nvidia_uvm && rmmod nvidia && modprobe nvidia && modprobe nvidia_uvm && modprobe nvidia_modeset
 
-docker exec fis-hubu-consumer-$ID /bin/bash -c "cd app && python consumer.py --kafkahost='kafka_kafka_1:29092' --output=/mnt --topic=video-stream-patient-$ID --sparkhost='spark://spark-master-fishubu:7077' $OPTIONSCONSUMER"
+docker exec -d fis-hubu-consumer-$ID /bin/bash -c "cd app && python consumer.py --kafkahost='kafka_kafka_1:29092' --output=/mnt --topic=video-stream-patient-$ID --sparkhost='spark://spark-master-fishubu:7077' $OPTIONSCONSUMER"
 echo 'Lanzado consumidor'
 docker exec -d fis-hubu-productor-$ID /bin/bash -c "cd app && python producer.py --port=12345 --kafkahost='kafka_kafka_1:9092' --topic=video-stream-patient-$ID"
 echo 'Lanzado productor'
