719ac9ca5d16fa1a83ef66ae81658f03c0e5f6b2
José Luis Garrido Labrador <jlgarrido97@gmail.com>
2020-07-05 00:41:54 +0200

    Kafka port correction

--- src/scripts/helpers/launch_stream.sh
+++ src/scripts/helpers/launch_stream.sh
@@ -10,7 +10,7 @@ OPTIONSCONSUMER="$3"
 
 docker exec -d fis-hubu-consumer-$ID /bin/bash -c "cd app && /root/miniconda3/envs/default-conda/bin/python consumer.py --kafkahost='kafka_kafka_1:29092' --output=/mnt --topic=video-stream-patient-$ID --sparkhost='spark://spark-master-fishubu:7077' $OPTIONSCONSUMER"
 echo 'Lanzado consumidor'
-docker exec -d fis-hubu-productor-$ID /bin/bash -c "cd app && /root/miniconda3/envs/default-conda/bin/python producer.py --port=12345 --kafkahost='kafka_kafka_1:9092' --topic=video-stream-patient-$ID"
+docker exec -d fis-hubu-productor-$ID /bin/bash -c "cd app && /root/miniconda3/envs/default-conda/bin/python producer.py --port=12345 --kafkahost='kafka_kafka_1:29092' --topic=video-stream-patient-$ID"
 echo 'Lanzado productor'
 docker exec -d fis-hubu-productor-$ID /bin/bash -c "cd app && /root/miniconda3/envs/default-conda/bin/python emitter.py --port=12345 --file=/app/testvideos/sentado2-cruzado.mp4 $OPTIONSEMITTER"
 echo 'Lanzado emisor para test'
