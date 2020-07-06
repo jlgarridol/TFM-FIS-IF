a085f5666ce6a5021d4f66927f8398a35f9feda1
José Luis Garrido Labrador <jlgarrido@ubu.es>
2020-07-06 03:26:03 +0200

    Cuda compiled correctly

--- src/process/consumer.py
+++ src/process/consumer.py
@@ -160,7 +160,7 @@ def op(package):
             traceback.print_exc()
             lg.debug(traceback.format_exc())
             lg.error("Hubo un error procesando la imagen "+str(k))
-            with open("/logs/mylog","a") as f:
+            with open("/logs/notprocesslog","a") as f:
                 f.writelines(traceback.format_exc())
     return k, img
 
